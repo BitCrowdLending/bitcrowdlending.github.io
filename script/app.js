2aa77e621b5f9fb6d5f3da9dfec34fcce683e0b8
Cesar Azevedo <cesardeazevedo@outlook.com>
2015-06-07 15:13:28 -0300

    deploy

--- script/app.js
+++ script/app.js
@@ -2,25 +2,25 @@ $(document).ready(function(){
 
     var jarallax = new Jarallax();
 
-    $("#home").css("height", window.innerHeight - 90 + "px");
+    $("#home").css("height", window.innerHeight - (window.innerHeight / 2) + 150 + "px");
 
     jarallax.addAnimation("#logo_img", [
           { progress : "0%",  marginTop : "0px", opacity: "1" }
-        , { progress : "100%", marginTop : "400px", opacity : "-0.5" }
+        , { progress : "100%", marginTop : "550px", opacity : "-0.5" }
     ]);
 
     var point = function(element, ToClass) {
         var footerPoint = new Waypoint({
-            element : element
+              element : element
             , handler : function(direction){
 
                 element.fadeTo("fast", 1);
                 if(typeof ToClass !== 'undefined')
                     element.addClass(ToClass);
             }
-            , offset: '60%'
+            , offset: '75%'
         });
-    }
+    };
 
     point($("#about"));
     point($("#highlight--left"), "highlight--in");
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
     var scrollTo = function(to) {
         $.scrollTo(to, 1000, {easing:'easeInOutExpo',offset:0,'axis':'y'});
-    }
+    };
 
     $("#scroll-about").click(function(){
         scrollTo("#about");
